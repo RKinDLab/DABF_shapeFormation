ca215c4af3074f50f343e08cb678f3eed347c5ad
Farid Sahebsara <fsahebsa@gmu.edu>
2025-03-29 17:01:57 -0400

    added run_simulation_2D script, to initialize MATLAB env, solve ODE, and plot the results

---
+++ init.m
@@ -0,0 +1,14 @@
+% include directories
+addpath(fullfile(pwd, 'util'));
+addpath(fullfile(pwd, 'plot'));
+
+% setup desired framework
+radius = 0.3/2;
+num_agents = 5;
+desired_pos = desiredShapeCoordinate(num_agents,radius);
+
+% initial condition of agents
+initial_pos = [0.078506534833198,0.010161761543734,0.144455924557333,-0.042617455513893,0.096503177381683;-0.162471989290054,0.012137687357145,-0.006058666579159,0.068572455869610,0.008020986956155];
+
+% define control gains
+alpha = 5*ones(num_agents,1);
---
+++ run_simulation_2D.m
@@ -0,0 +1,5 @@
+clear; close all; clc;
+run init.m %initialize MATLAB env
+run Shape_Formation_2D.m
+%run Shape_Formation.m
+run plots.m
